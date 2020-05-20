bf2653a27bd76b95c86cf859a71f9a3b35b8a0d6
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-05-19 21:53:28 -0600

    Added the BasicUI.java file for the javaFX application, integrated SceneBuilder to the IDE, TODO: create the basic interface for the active sprint.

---
+++ src/com/partyInterface/BasicUI.java
@@ -0,0 +1,16 @@
+package com.partyInterface;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BasicUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        //TODO utilize the javaFX application with SceneBuilder to format the basic interface for the project
+    }
+}
