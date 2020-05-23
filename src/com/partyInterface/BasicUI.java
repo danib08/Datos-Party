b952ae39e4d56f05aa7e63574bc7a83a5f3ceb56
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-05-23 02:09:35 -0600

    basic-mainboard-interface-created
    The .fxml file for the basic, main board and its controller class were modified to a usable point -marked as done in the project management platform-, this javaFX application has not been linked to any project logic yet, and is only a preliminary runnable version of the desired interface.

--- src/com/partyInterface/BasicUI.java
+++ src/com/partyInterface/BasicUI.java
@@ -1,17 +1,41 @@
 package com.partyInterface;
 
+import com.structures.CircularSinglyLinkedList;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class BasicUI extends Application {
 
     public static void main(String[] args) {
         launch(args);
+
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        /* TODO utilize the javaFX application with SceneBuilder to
-            format the basic interface for the project */
+    public void start(Stage primaryStage) throws IOException{
+        // Creating the loader
+        FXMLLoader loader = new FXMLLoader();
+        // path to the file
+        String filePath = "C:\\Users\\Alejandro José\\IdeaProjects\\Datos-Party\\src\\com\\partyInterface\\Sample.fxml";
+        FileInputStream fxmlStream = new FileInputStream(filePath);
+
+        //Create pane and all Details
+        AnchorPane root = (AnchorPane) loader.load(fxmlStream);
+
+        //Create Scene
+        Scene scene = new Scene(root);
+        //set the scene to the stage
+        primaryStage.setScene(scene);
+        //set the title to the stage
+        primaryStage.setTitle("Main Board");
+        //display the stage
+        primaryStage.show();
     }
 }
