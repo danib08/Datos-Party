8ff11e4d06d6401d31cb9f2e28c31875b9c157ef
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-05-31 19:08:28 -0600

    added-event-methods
    most events have been created as callable methods that will modify the players' attributes related to in-game currencies or positions. A new method had to be implemented in the Player class in order to allow Events class to update the players' stars amount.

--- src/com/gameLogic/Events.java
+++ src/com/gameLogic/Events.java
@@ -1,5 +1,69 @@
 package com.gameLogic;
 import com.gameLogic.Player;
+import com.structures.Square;
+
 public class Events {
-    
+
+    public void winStars(Player player){
+        player.updateStars(1);
+        System.out.println("player won a star!");
+    }
+
+    public void loseStars(Player player){
+        int star = player.getStars();
+        if (star> 0){
+            player.updateStars(-1);
+            System.out.println("player lost a star!");
+        }
+        else{
+            System.out.println("player cannot lose any stars.");
+        }
+    }
+
+    public void stealStar(Player stealer, Player target){
+        int stealableStar = target.getStars();
+        if (stealableStar > 0){
+            stealer.updateStars(1);
+            target.updateStars(-1);
+            System.out.println("stealer got a star from the target");
+        }
+        else{
+            System.out.println("target has no stealable stars.");
+        }
+    }
+
+    public void donateCoins(Player donor, Player target){
+        int coins = donor.getCoins();
+        if (coins >= 5){
+            donor.updateCoins(-5);
+            target.updateCoins(5);
+            System.out.println("donor gave 5 coins to target");
+        }
+        else{
+            System.out.println("donor has insufficient coin balance");
+        }
+    }
+
+    public void stealCoins(Player stealer, Player target){
+        int coins = target.getCoins();
+        if (coins >= 5){
+            stealer.updateCoins(5);
+            target.updateCoins(-5);
+            System.out.println("stealer stole coins from target!");
+        }
+    }
+
+    public void teleport(Player player, Square position){
+        player.setPosition(position);
+        System.out.println("player teleported to new square!");
+    }
+
+    public void swap(Player player1, Player player2){
+        Square p1Pos= player1.getPosition();
+        Square p2Pos = player2.getPosition();
+        player1.setPosition(p2Pos);
+        player2.setPosition(p1Pos);
+        System.out.println("player1 n player2 swapped positions!");
+    }
+
 }
