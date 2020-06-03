9d0611146325c6fa5381d243a9b4f4b6b5f9446f
Jean Franco Gondrez <jean.gondrez@plecsys.net>
2020-06-03 17:36:48 -0600

    Added-comments
    Commented the Main class

--- src/com/gameLogic/Main.java
+++ src/com/gameLogic/Main.java
@@ -166,18 +166,22 @@ public class Main {
 
         //shuffle method is used to "randomize" the eventList.
         Collections.shuffle(eventList);
-        
+
+        // Adds the events to the stack.
         for (Integer event : eventList) {
             eventStack.prepend(event);
         }
 
+        // The user can choose the number of players.
         Scanner scan = new Scanner(System.in);
         System.out.println("Choose number of players: 2, 3 o 4");
         int numberPlayer = scan.nextInt();
         Player[] playerArray = new Player[numberPlayer];
 
+        // Creates the Event class to select events.
         Events eventHandler = new Events(playerArray, pathArray, eventStack, eventList);
 
+        // The user can input the name of the players
         for (int i = 0; i < numberPlayer; i++){
             System.out.println("Enter the name of the player #" + (i+1) +" : ");
             if (i == 0){
@@ -187,16 +191,19 @@ public class Main {
             playerArray[i] = new Player(name, mainBoard.getHead(), mainBoard, eventHandler, star);
         }
 
+        // Selects the number of rounds to be played
         int roundsPlayed = 0;
         System.out.println("Enter number of rounds: ");
         int roundsTotal = scan.nextInt();
 
-        // For the minigame
+        // Variables used to select the minigames
         int lastPlayed = 0;
         int chooseMinigame;
 
+        // Starts the game logic
         while (roundsPlayed < roundsTotal ){
             for (Player player : playerArray){
+
                 // Player round logic goes here
                 System.out.println(player.getName());
                 System.out.println("Press enter to move");
@@ -204,12 +211,16 @@ public class Main {
                     scan.nextLine();
                 }
                 scan.nextLine();
+
+                // Rolls the die and moves the player accordingly
                 player.move(playerArray);
             }
 
+            // Selects a random integer that will be the next minigame to be played.
             Random random = new Random();
             chooseMinigame = random.nextInt(6) + 1;
 
+            // Checks that the chosen minigame isn't the same that was last played.
             while (lastPlayed == chooseMinigame){
                 System.out.println("Selecting another minigame");
                 chooseMinigame = random.nextInt(6) + 1;
@@ -217,6 +228,7 @@ public class Main {
 
             lastPlayed = chooseMinigame;
 
+            // This is a switch to select the next minigame to be played.
             switch (chooseMinigame){
                 case 1:
                     System.out.println("Play 1st Minigame");
