cc894c050dd41c4c9f3169ac3bdf050eac7be7fc
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-06-03 17:30:25 -0600

    added-main-comments
    brief explanations of what the Main.main() adds to the whole project.

--- src/com/gameLogic/Main.java
+++ src/com/gameLogic/Main.java
@@ -12,18 +12,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        //here the different lists are created
+        //the mainBoard represents the outer border for the path players will see.
         CircularSinglyLinkedList mainBoard = new CircularSinglyLinkedList();
+
+        //pathA,B and C are the secondary paths that players can opt to cross over to.
         SinglyLinkedList pathA = new SinglyLinkedList();
         SinglyLinkedList pathB = new SinglyLinkedList();
         DoublyLinkedList pathC = new DoublyLinkedList();
+
+        //the path D is the one that can only be accessed through 'teleporting'.
         CircularDoublyLinkedList pathD = new CircularDoublyLinkedList();
+
+        //a List-type array is made with the five paths.
         List[] pathArray = {mainBoard, pathA, pathB, pathC, pathD};
 
+        //the star is created with a specific method, not regularly instantiated
+        //because this object is accessed through a Singleton design pattern
         Star star = Star.getStar(pathArray);
 
+        //A stack is implemented using the SinglyLinkedList structure, this is meant to
+        //hold the representation of events to be accessed by the game logic.
         SinglyLinkedList eventStack = new SinglyLinkedList();
+
+        //The collections class ArrayList is used to create a randomizable structure that can
+        //later be accessed by the main() method to add the random-ordered elements to the event stack.
         ArrayList<Integer> eventList = new ArrayList<>();
 
+        //As the game board follows a strictly defined order, the paths are filled with the respective
+        //element that will tell the game logic what type of "Square" or position the players are on.
         mainBoard.append(1);
         mainBoard.append(2);
         mainBoard.append(3);
@@ -88,6 +106,8 @@ public class Main {
         pathD.append(4);
         pathD.append(4);
 
+        //the ArrayList object is used as described previously, to add a certain amount of numbers
+        //from 1 to 9 that will sequentially be randomized
         eventList.add(1);
         eventList.add(1);
         eventList.add(1);
@@ -143,8 +163,10 @@ public class Main {
         eventList.add(9);
         eventList.add(9);
         eventList.add(9);
-        Collections.shuffle(eventList);
 
+        //shuffle method is used to "randomize" the eventList.
+        Collections.shuffle(eventList);
+        
         for (Integer event : eventList) {
             eventStack.prepend(event);
         }
