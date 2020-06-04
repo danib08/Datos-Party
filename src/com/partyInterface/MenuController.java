c3c63dd06d6f304acce7d86472af0390f483dab0
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-06-03 20:31:13 -0600

    interface-test-changes

--- src/com/partyInterface/MenuController.java
+++ src/com/partyInterface/MenuController.java
@@ -1,10 +1,15 @@
 package com.partyInterface;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 public class MenuController extends Application {
 
@@ -21,27 +26,64 @@ public class MenuController extends Application {
      * NOTE: This method is called on the JavaFX Application Thread.
      * </p>
      *
-     * @param primaryStage the primary stage for this application, onto which
+     * @param stage the primary stage for this application, onto which
      *                     the application scene can be set. The primary stage will be embedded in
      *                     the browser if the application was launched as an applet.
      *                     Applications may create other stages, if needed, but they will not be
      *                     primary stages and will not be embedded in the browser.
      */
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
         // Creating the loader, the pane and all details
-        Parent menu = FXMLLoader.load(getClass().getResource("MenuUI.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuUI.fxml"));
+        Parent menu = loader.load();
         // Create scene
-        primaryStage.setScene(new Scene(menu));
+        stage.setScene(new Scene(menu));
 
         // Set the title to the stage
-        primaryStage.setTitle("Main Menu");
+        stage.setTitle("Main Menu");
 
         // Set resizable options
-        primaryStage.setResizable(false);
+        stage.setResizable(false);
 
         // Displays the stage
-        primaryStage.show();
+        stage.show();
         //TODO make the button summon the main board.
+
+        /*
+        Button showButton = new Button("game");
+        showButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                stage.hide();
+                try{
+                    Parent game = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+                    Stage gameSt = new Stage();
+                    gameSt.setScene(new Scene(game));
+                    gameSt.setTitle("gameboard");
+                    gameSt.setResizable(false);
+                    gameSt.show();
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+        });
+        */
+    }
+
+    public void ButtonClick(){
+        try{
+            Parent game = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+            Stage gameSt = new Stage();
+            gameSt.setScene(new Scene(game));
+            gameSt.setTitle("gameboard");
+            gameSt.setResizable(false);
+            gameSt.show();
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
