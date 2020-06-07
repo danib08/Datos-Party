4540036c0a9d0ddca9378a511effb8a5bd80b3ec
Daniela Brenes <danibreo6@gmail.com>
2020-06-06 21:55:24 -0600

    return-to-menu-method
    Implemented the method to return to the menu when the buttons in the Info and Basics scenes are clicked

---
+++ src/com/partyInterface/InfoController.java
@@ -0,0 +1,30 @@
+package com.partyInterface;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class InfoController {
+
+
+    /**
+     * This method is called when the Return to Menu button is clicked, and shows the Menu scene
+     * @param event Receives a click on the button
+     * @throws IOException
+     */
+    public void changeToMenu(ActionEvent event) throws IOException{
+        Parent menuParent = FXMLLoader.load(getClass().getResource("MenuUI.fxml"));
+        Scene menuScene = new Scene(menuParent);
+
+        // This gets the stage information.
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(menuScene);
+        window.show();
+    }
+}
