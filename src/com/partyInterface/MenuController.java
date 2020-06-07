9a03b73e8b03f7dd5374f75e395c90efbaccc9c8
Jean Franco Gondrez <jean.gondrez@plecsys.net>
2020-06-06 20:48:36 -0600

    Revert "Update-MenuController"
    This reverts commit 6333c6aaf9cda44e65795024a3b09ca7605f8338.

--- src/com/partyInterface/MenuController.java
+++ src/com/partyInterface/MenuController.java
@@ -6,40 +6,83 @@ import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
-import java.io.IOException;
+public class MenuController extends Application {
 
-public class MenuController {
-
-    @FXML
-    Button infoButton;
-    @FXML
-    Button basicsButton;
-
-    public void changeToInfo(ActionEvent event) throws IOException {
-        Parent infoParent = FXMLLoader.load(getClass().getResource("GameInfo.fxml"));
-        Scene infoScene = new Scene(infoParent);
-
-        // This gets the stage information.
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(infoScene);
-        window.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public void changeToBasic(ActionEvent event) throws IOException{
-        Parent basicParent = FXMLLoader.load(getClass().getResource("BasicInfo.fxml"));
-        Scene basicScene = new Scene(basicParent);
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param stage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Creating the loader, the pane and all details
+        Parent menu = FXMLLoader.load(getClass().getResource("MenuUI.fxml"));
+        // Create scene
+        stage.setScene(new Scene(menu));
 
-        // This gets the stage information.
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        // Set the title to the stage
+        stage.setTitle("Main Menu");
 
-        window.setScene(basicScene);
-        window.show();
+        // Set resizable options
+        stage.setResizable(false);
+
+        // Displays the stage
+        stage.show();
+        //TODO make the button summon the main board.
+
+        /*
+        Button showButton = new Button("game");
+        showButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                stage.hide();
+                try{
+                    Parent game = FXMLLoader.load(getClass().getResource("gameBoard.fxml"));
+                    Stage gameSt = new Stage();
+                    gameSt.setScene(new Scene(game));
+                    gameSt.setTitle("gameboard");
+                    gameSt.setResizable(false);
+                    gameSt.show();
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+        });
+        */
+    }
+
+    public void ButtonClick(){
+        try{
+            Parent game = FXMLLoader.load(getClass().getResource("gameBoard.fxml"));
+            Stage gameSt = new Stage();
+            gameSt.setScene(new Scene(game));
+            gameSt.setTitle("gameboard");
+            gameSt.setResizable(false);
+            gameSt.show();
+
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
