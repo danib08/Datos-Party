0048f7da7243c8417fe190ab44571da6c4c2ba58
Jean Franco Gondrez <jean.gondrez@plecsys.net>
2020-06-06 20:53:50 -0600

    Updated MenuController
    Updated the menu controller so the user can change from the Menu view to the Info view or the Basics view.

--- src/com/partyInterface/MenuController.java
+++ src/com/partyInterface/MenuController.java
@@ -1,5 +1,42 @@
 package com.partyInterface;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class MenuController {
 
+    @FXML
+    Button infoButton;
+    @FXML
+    Button basicsButton;
+
+    public void changeToInfo(ActionEvent event) throws IOException {
+        Parent infoParent = FXMLLoader.load(getClass().getResource("GameInfo.fxml"));
+        Scene infoScene = new Scene(infoParent);
+
+        // This gets the stage information.
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(infoScene);
+        window.show();
+    }
+
+    public void changeToBasic(ActionEvent event) throws IOException{
+        Parent basicParent = FXMLLoader.load(getClass().getResource("BasicInfo.fxml"));
+        Scene basicScene = new Scene(basicParent);
+
+        // This gets the stage information.
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(basicScene);
+        window.show();
+    }
 }
