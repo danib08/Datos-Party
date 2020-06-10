2ba58296d166b2edeb44db0f69ecb67cc4419c05
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-06-09 23:48:43 -0600

    bomb-minigame-modifications
    this commit is only related to the bomb minigame.

--- src/com/minigames/bombGame/BombGame2.java
+++ src/com/minigames/bombGame/BombGame2.java
@@ -5,34 +5,25 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class BombGame2 implements Initializable {
+public class BombGame2 {
     @FXML ImageView player1bomb;
     @FXML ImageView player2bomb;
     @FXML ImageView player3bomb;
     @FXML ImageView player4bomb;
     Player[] players;
-    Image explosion = new Image("explosion.png");
 
-    /**
-     * Called to initialize a controller after its root element has been
-     * completely processed.
-     *
-     * @param location  The location used to resolve relative paths for the root object, or
-     *                  <tt>null</tt> if the location is not known.
-     * @param resources The resources used to localize the root object, or <tt>null</tt> if
-     */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        int pAmount = players.length;
-        //TODO bring the player array to the game window
 
-    }
 
-    public void showDetonation(){
-        //TODO fix the url to the images to test the button.
+    public void showDetonation() throws FileNotFoundException {
+
+        FileInputStream input = new FileInputStream("src/com/images/explosion.png");
+        Image explosion = new Image(input);
         player1bomb.setImage(explosion);
         player2bomb.setImage(explosion);
         player3bomb.setImage(explosion);
