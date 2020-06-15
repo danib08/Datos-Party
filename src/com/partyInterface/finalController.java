af8741f9b1840208351bf05d3a0b544b3304fa4f
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-06-14 20:52:35 -0600

    view-controller-relation-added-for-game-finalization
    created the fxml for the view and a controller class alredy coded to work together as the final window showed in the game, that displays them in order and gives the user an option to close the game.
    extra: deleted a test class that is no longer needed in one of the minigame subpackages.

---
+++ src/com/partyInterface/finalController.java
@@ -0,0 +1,91 @@
+package com.partyInterface;
+
+import com.gameLogic.Player;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class finalController {
+    //name fields
+    @FXML TextField p1nameField;
+    @FXML TextField p2nameField;
+    @FXML TextField p3nameField;
+    @FXML TextField p4nameField;
+
+    //star fields
+    @FXML TextField p1starsField;
+    @FXML TextField p2starsField;
+    @FXML TextField p3starsField;
+    @FXML TextField p4starsField;
+
+    //coin fields
+    @FXML TextField p1coinsField;
+    @FXML TextField p2coinsField;
+    @FXML TextField p3coinsField;
+    @FXML TextField p4coinsField;
+
+    @FXML Button closeButton;
+
+    Player [] players;
+    int pAmount;
+
+    public void initData(Player[] playerArr){
+        this.players = playerArr;
+        this.pAmount = this.players.length;
+
+        //setting the name values
+        p1nameField.setText(players[0].getName());
+        p2nameField.setText(players[1].getName());
+
+        //setting the star and coin values
+        p1starsField.setText(Integer.toString(players[0].getStars()));
+        p1coinsField.setText(Integer.toString(players[0].getCoins()));
+
+        p2starsField.setText(Integer.toString(players[1].getStars()));
+        p2coinsField.setText(Integer.toString(players[1].getCoins()));
+
+        //in case that there are only two players, sets the Textfields for p3 and p4 as hidden.
+        if (pAmount == 2){
+            p3nameField.setVisible(false);
+            p3coinsField.setVisible(false);
+            p3starsField.setVisible(false);
+
+            p4nameField.setVisible(false);
+            p4starsField.setVisible(false);
+            p4coinsField.setVisible(false);
+        }
+
+        if (pAmount >= 3){
+            p3nameField.setText(players[2].getName());
+
+            p3starsField.setText(Integer.toString(players[2].getStars()));
+            p3coinsField.setText(Integer.toString(players[2].getCoins()));
+
+            //as there are actually three players for this case, the respective fields have to be shown.
+            p3nameField.setVisible(true);
+            p3coinsField.setVisible(true);
+            p3starsField.setVisible(true);
+            if (pAmount == 4){
+                p4nameField.setText(players[3].getName());
+
+                p4starsField.setText(Integer.toString(players[3].getStars()));
+                p4coinsField.setText(Integer.toString(players[3].getCoins()));
+
+                //as there are a true max of players (4), the respective fields are shown.
+                p4nameField.setVisible(true);
+                p4starsField.setVisible(true);
+                p4coinsField.setVisible(true);
+            }
+        }
+    }
+
+    public void closeWindow(ActionEvent buttonClick){
+        //obtains the Stage(window) in which the scene is loaded.
+        Stage window = (Stage) ((Node)buttonClick.getSource()).getScene().getWindow();
+        window.close();
+    }
+
+}
